4fbc5cadca18e1224f90f3e9c961fe7e5a5a71d5
Wanderson Castro <wandersonce1@hotmail.com>
2024-02-22 12:53:12 -0800

    Creating the settings to header

---
+++ src/components/Header.tsx
@@ -0,0 +1,22 @@
+import { createClient } from '@/prismicio';
+import Link from 'next/link';
+
+export default async function Header() {
+  const client = createClient();
+  const settings = await client.getSingle('settings');
+
+  return (
+    <div className="top-0 z-50 mx-auto mx-7xl md:sticky md:top-4">
+      <nav>
+        <ul>
+          <li>
+            <Link href="/" aria-label="Home Page">
+              {settings.data.name}
+            </Link>
+          </li>
+          <li></li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
