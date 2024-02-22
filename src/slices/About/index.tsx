d3ba5f75c7859354a8f20942848404fed9e762cb
Wanderson Castro <wandersonce1@hotmail.com>
2024-02-22 13:45:05 -0800

    Adding the button component

--- src/slices/About/index.tsx
+++ src/slices/About/index.tsx
@@ -1,6 +1,8 @@
 import Bounded from '@/components/Bounded';
+import Button from '@/components/Button';
+import Heading from '@/components/Heading';
 import { Content } from '@prismicio/client';
-import { SliceComponentProps } from '@prismicio/react';
+import { PrismicRichText, SliceComponentProps } from '@prismicio/react';
 
 /**
  * Props for `About`.
@@ -16,7 +18,18 @@ const About = ({ slice }: AboutProps): JSX.Element => {
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      <div className="grid gap-x-8 gap-y-6 md:grid-cols-[2fr, 1fr]"></div>
+      <div className="grid1 gap-x-8 gap-y-6 md:grid-cols-[2fr, 1fr]">
+        <Heading as="h1" size="xl" className="col-start-1">
+          {slice.primary.heading}
+        </Heading>
+        <div className="prose prose-xl prose-slate prose-invert col-start-1">
+          <PrismicRichText field={slice.primary.description} />
+        </div>
+        <Button
+          linkField={slice.primary.button_link}
+          label={slice.primary.button_text}
+        />
+      </div>
     </Bounded>
   );
 };
