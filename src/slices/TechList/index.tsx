53aae2534c7bc36106a8dd5ecd590972c50b0fec
Wanderson Castro <wandersonce1@hotmail.com>
2024-02-23 09:03:06 -0800

    Adding tech list items

--- src/slices/TechList/index.tsx
+++ src/slices/TechList/index.tsx
@@ -1,5 +1,16 @@
-import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+'use client';
+
+import { Content } from '@prismicio/client';
+import { SliceComponentProps } from '@prismicio/react';
+import React from 'react';
+import { MdCircle } from 'react-icons/md';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+import Bounded from '@/components/Bounded';
+import Heading from '@/components/Heading';
+
+gsap.registerPlugin(ScrollTrigger);
 
 /**
  * Props for `TechList`.
@@ -15,7 +26,35 @@ const TechList = ({ slice }: TechListProps): JSX.Element => {
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      Placeholder component for tech_list (variation: {slice.variation}) Slices
+      <Bounded as="div">
+        <Heading size="xl" as="h2" className="mb-8">
+          {slice.primary.heading}
+        </Heading>
+      </Bounded>
+
+      {slice.items.map(({ tech_color, tech_name }, index) => (
+        <div
+          key={index}
+          className="tech-row mb-8 flex items-center justify-center gap-4 text-slate-700"
+          aria-label={tech_name || undefined}
+        >
+          {Array.from({ length: 15 }, (_, index) => (
+            <React.Fragment key={index}>
+              <span
+                className="tech-item text-8xl font-extrabold uppercase tracking-tighter"
+                style={{
+                  color: index === 7 && tech_color ? tech_color : 'inherit',
+                }}
+              >
+                {tech_name}
+              </span>
+              <span className="text-3xl">
+                <MdCircle />
+              </span>
+            </React.Fragment>
+          ))}
+        </div>
+      ))}
     </section>
   );
 };
