6346369d703df9f04e85ec4365a56ff5029fe99a
Wanderson Castro <wandersonce1@hotmail.com>
2024-02-26 11:18:11 -0800

    adding prettier tailwind

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,15 @@
-import type { Metadata } from 'next';
-import { Urbanist } from 'next/font/google';
-import './globals.css';
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
-import clsx from 'clsx';
+import type { Metadata } from "next";
+import { Urbanist } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import clsx from "clsx";
 
-const urbanist = Urbanist({ subsets: ['latin'] });
+const urbanist = Urbanist({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Wanderson Castro',
-  description: 'Web Portfolio',
+  title: "Wanderson Castro",
+  description: "Web Portfolio",
 };
 
 export default function RootLayout({
@@ -22,14 +22,14 @@ export default function RootLayout({
       <body
         className={clsx(
           urbanist.className,
-          'relative min-h-screen overflow-x-hidden'
+          "relative min-h-screen overflow-x-hidden",
         )}
       >
         <Header />
         {children}
         <Footer />
-        <div className="absolute inset-0 -z-50 max-h-screen background-gradient"></div>
-        <div className='absolute pointer-events-none inset-0 -z-40 h-full bg-[url("/noiseGif.gif")] opacity-10 mix-blend-soft-light'></div>
+        <div className="background-gradient absolute inset-0 -z-50 max-h-screen"></div>
+        <div className='pointer-events-none absolute inset-0 -z-40 h-full bg-[url("/noiseGif.gif")] opacity-10 mix-blend-soft-light'></div>
       </body>
     </html>
   );
--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,13 +1,13 @@
-'use client';
+"use client";
 
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef } from "react";
 
-import { Content, KeyTextField } from '@prismicio/client';
-import { SliceComponentProps } from '@prismicio/react';
-import { gsap } from 'gsap';
-import Bounded from '@/components/Bounded';
+import { Content, KeyTextField } from "@prismicio/client";
+import { SliceComponentProps } from "@prismicio/react";
+import { gsap } from "gsap";
+import Bounded from "@/components/Bounded";
 
-import Shapes from './Shapes';
+import Shapes from "./Shapes";
 
 /**
  * Props for `Hero`.
@@ -25,7 +25,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       const tl = gsap.timeline();
 
       tl.fromTo(
-        '.name-animation',
+        ".name-animation",
         {
           x: -100,
           opacity: 0,
@@ -35,18 +35,18 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
           x: 0,
           opacity: 1,
           rotate: 0,
-          ease: 'elastic.out(1,0.3)',
+          ease: "elastic.out(1,0.3)",
           duration: 1,
-          transformOrigin: 'left top',
+          transformOrigin: "left top",
           stagger: {
             each: 0.1,
-            from: 'random',
+            from: "random",
           },
-        }
+        },
       );
 
       tl.fromTo(
-        '.job-title',
+        ".job-title",
         {
           y: 20,
           opacity: 0,
@@ -57,8 +57,8 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
           y: 0,
           duration: 0.8,
           scale: 1,
-          ease: 'elastic.out(1,0.3)',
-        }
+          ease: "elastic.out(1,0.3)",
+        },
       );
     }, component);
 
@@ -70,7 +70,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       return;
     }
 
-    return name.split('').map((letter, index) => (
+    return name.split("").map((letter, index) => (
       <span
         key={index}
         className={`name-animation name-animation-${key} inline-block opacity-0`}
@@ -86,23 +86,23 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       data-slice-variation={slice.variation}
       ref={component}
     >
-      <div className="grid min-h-[70vh] grid-cols-1 md:grid-cols-2 items-center">
+      <div className="grid min-h-[70vh] grid-cols-1 items-center md:grid-cols-2">
         <Shapes />
         <div className="col-start-1 md:row-start-1">
           <h1
             className="mb-8 text-[clamp(3rem,12vmin,16rem)] font-extrabold leading-none tracking-tighter"
             aria-label={
-              slice.primary.first_name + ' ' + slice.primary.last_name
+              slice.primary.first_name + " " + slice.primary.last_name
             }
           >
             <span className="block text-slate-300">
-              {renderLetters(slice.primary.first_name, 'first')}
+              {renderLetters(slice.primary.first_name, "first")}
             </span>
             <span className="-mt-[.2em] block text-slate-500">
-              {renderLetters(slice.primary.last_name, 'last')}
+              {renderLetters(slice.primary.last_name, "last")}
             </span>
           </h1>
-          <span className="job-title block bg-gradient-to-tr opacity-0 from-yellow-500 via-yellow-200 to-yellow-500 bg-clip-text text-2xl font-bold uppercase tracking-[.2em] text-transparent opacity-100 md:text-4xl">
+          <span className="job-title block bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500 bg-clip-text text-2xl font-bold uppercase tracking-[.2em] text-transparent opacity-0 opacity-100 md:text-4xl">
             {slice.primary.tag_line}
           </span>
         </div>
