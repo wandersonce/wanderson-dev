2d65c3633054075f33d029c79106faad7625aaa9
Wanderson Castro <wandersonce1@hotmail.com>
2024-02-26 10:25:18 -0800

    Adding noise /footer and fixing css

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,27 +1,13 @@
 import { createClient } from '@/prismicio';
-import { PrismicNextLink } from '@prismicio/next';
-import Link from 'next/link';
+import NavBar from './Navbar';
 
 export default async function Header() {
   const client = createClient();
   const settings = await client.getSingle('settings');
 
   return (
-    <div className="top-0 z-50 mx-auto mx-7xl md:sticky md:top-4">
-      <nav>
-        <ul>
-          <li>
-            <Link href="/" aria-label="Home Page">
-              {settings.data.name}
-            </Link>
-          </li>
-          {settings.data.nav_item.map(({ link, label }, index) => (
-            <li key={index}>
-              <PrismicNextLink field={link}>{label}</PrismicNextLink>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </div>
+    <header className="top-0 z-50 mx-auto max-w-7xl md:sticky md:top-4">
+      <NavBar settings={settings} />
+    </header>
   );
 }
