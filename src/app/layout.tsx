4e96811b10860d3bc27912dc05d99ec0ad6d0545
Wanderson Castro <wandersonce1@hotmail.com>
2024-02-26 11:25:59 -0800

    Getting meta data from prismic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,13 +4,19 @@ import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import clsx from "clsx";
+import { PrismicPreview } from "@prismicio/next";
+import { createClient, repositoryName } from "@/prismicio";
 
 const urbanist = Urbanist({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Wanderson Castro",
-  description: "Web Portfolio",
-};
+export async function generateMetadata(): Promise<Metadata> {
+  const client = createClient();
+  const settings = await client.getSingle("settings");
+  return {
+    title: settings.data.meta_title,
+    description: settings.data.meta_description,
+  };
+}
 
 export default function RootLayout({
   children,
@@ -31,6 +37,7 @@ export default function RootLayout({
         <div className="background-gradient absolute inset-0 -z-50 max-h-screen"></div>
         <div className='pointer-events-none absolute inset-0 -z-40 h-full bg-[url("/noiseGif.gif")] opacity-10 mix-blend-soft-light'></div>
       </body>
+      <PrismicPreview repositoryName={repositoryName} />
     </html>
   );
 }
