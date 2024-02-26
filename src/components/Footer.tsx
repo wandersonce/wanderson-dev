f0700c2d4c6960b58dbef2c39533ccc8d0e853c1
Wanderson Castro <wandersonce1@hotmail.com>
2024-02-26 11:35:56 -0800

    Fixing gitbhub/email link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,18 @@
-import clsx from 'clsx';
-import React from 'react';
-import { createClient } from '@/prismicio';
-import { PrismicNextLink } from '@prismicio/next';
-import Link from 'next/link';
-import Bounded from '@/components/Bounded';
-import { isFilled } from '@prismicio/client';
-import { FaGithub, FaTwitter, FaLinkedin, FaEnvelope } from 'react-icons/fa6';
+import clsx from "clsx";
+import React from "react";
+import { createClient } from "@/prismicio";
+import { PrismicNextLink } from "@prismicio/next";
+import Link from "next/link";
+import Bounded from "@/components/Bounded";
+import { isFilled } from "@prismicio/client";
+import { FaGithub, FaTwitter, FaLinkedin, FaEnvelope } from "react-icons/fa6";
 
 export default async function Footer() {
   const client = createClient();
-  const settings = await client.getSingle('settings');
+  const settings = await client.getSingle("settings");
   return (
-    <Bounded as="footer" className="text-slate-600 lg:pt-5 lg:pb-0">
-      <div className="container mx-auto mt-20 flex flex-col items-center justify-between gap-6 py-8 md:pt-0 md:mt-4 sm:flex-row ">
+    <Bounded as="footer" className="text-slate-600 lg:pb-0 lg:pt-5">
+      <div className="container mx-auto mt-20 flex flex-col items-center justify-between gap-6 py-8 sm:flex-row md:mt-4 md:pt-0 ">
         <div className="name flex flex-col items-center justify-center gap-x-4 gap-y-2 sm:flex-row sm:justify-self-start">
           <Link
             href="/"
@@ -37,7 +37,7 @@ export default async function Footer() {
                 <li>
                   <PrismicNextLink
                     className={clsx(
-                      'group relative block overflow-hidden  rounded px-3 py-1 text-base font-bold text-slate-100 transition-colors duration-150 hover:hover:text-yellow-400'
+                      "group relative block overflow-hidden  rounded px-3 py-1 text-base font-bold text-slate-100 transition-colors duration-150 hover:hover:text-yellow-400",
                     )}
                     field={link}
                   >
@@ -57,20 +57,20 @@ export default async function Footer() {
           </ul>
         </nav>
         <div className="socials inline-flex justify-center sm:justify-end">
-          {isFilled.link(settings.data.email_link) && (
-            <PrismicNextLink
-              field={settings.data.email_link}
-              className="p-2 text-2xl text-slate-300 transition-all duration-150 hover:scale-125 hover:text-yellow-400"
-              aria-label={settings.data.name + ' on GitHub'}
-            >
-              <FaGithub />
-            </PrismicNextLink>
-          )}
           {isFilled.link(settings.data.github_link) && (
             <PrismicNextLink
               field={settings.data.github_link}
               className="p-2 text-2xl text-slate-300 transition-all duration-150 hover:scale-125 hover:text-yellow-400"
-              aria-label={settings.data.name + ' Email'}
+              aria-label={settings.data.name + " on GitHub"}
+            >
+              <FaGithub />
+            </PrismicNextLink>
+          )}
+          {isFilled.link(settings.data.email_link) && (
+            <PrismicNextLink
+              field={settings.data.email_link}
+              className="p-2 text-2xl text-slate-300 transition-all duration-150 hover:scale-125 hover:text-yellow-400"
+              aria-label={settings.data.name + " Email"}
             >
               <FaEnvelope />
             </PrismicNextLink>
@@ -79,7 +79,7 @@ export default async function Footer() {
             <PrismicNextLink
               field={settings.data.linkedin_link}
               className="p-2 text-2xl text-slate-300 transition-all duration-150 hover:scale-125 hover:text-yellow-400"
-              aria-label={settings.data.name + ' on LinkedIn'}
+              aria-label={settings.data.name + " on LinkedIn"}
             >
               <FaLinkedin />
             </PrismicNextLink>
