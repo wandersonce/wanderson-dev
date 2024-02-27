1f22c4e8e3aa15b383852db4509ebf999dfd601c
Wanderson Castro <wandersonce1@hotmail.com>
2024-02-27 10:50:08 -0800

    Adding github fetch component

---
+++ src/components/GithubList.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { LuLoader2 } from "react-icons/lu";
+
+interface Repository {
+  id: number;
+  name: string;
+  description: string;
+  language: string;
+  html_url: string;
+}
+
+export default function GithubList() {
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [loadingState, setLoadingState] = useState(false);
+
+  useEffect(() => {
+    setLoadingState(true);
+
+    fetch(
+      "https://api.github.com/users/wandersonce/repos?per_page=100&sort=updated",
+    )
+      .then((response) => response.json())
+      .then((data) => setRepositories(data));
+
+    setLoadingState(false);
+  }, []);
+
+  return (
+    <div>
+      {loadingState && (
+        <div className="flex items-center justify-center">
+          Loading...
+          <LuLoader2 className="h-8 w-8 animate-spin" />
+        </div>
+      )}
+    </div>
+  );
+}
